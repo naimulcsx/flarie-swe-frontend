0435ab9a9492481c482ce4c45344f255eefbcb59
Naimul Haque <naimulcsx@gmail.com>
2023-12-11 23:47:23 +0600

    test: Add unit test for CampaignBanner component

--- src/components/campaign-banner/CampaignBanner.test.tsx
+++ src/components/campaign-banner/CampaignBanner.test.tsx
@@ -1,18 +1,109 @@
-import { cleanup } from '@testing-library/react';
+import React from 'react';
+import { cleanup, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { CampaignBanner } from './CampaignBanner';
+import { TestElement } from '../../enums/TestElement';
+import { gameConfig } from '../../config/game-config';
+import { calc } from '../../utils/calc';
+import renderer from 'react-test-renderer';
+import 'jest-styled-components';
 
 afterEach(cleanup);
 
 describe('[GAME] CampaignBanner', () => {
-  it('SHOULD Render CampaignBanner with all the CSS properties from config file', () => {
-    //
+  it('SHOULD Render CampaignBanner with all the CSS properties from config file', async () => {
+    render(<CampaignBanner />);
+    const campaignBanner = screen.getByTestId(TestElement.CAMPAIGN_BANNER);
+    const tree = renderer.create(<CampaignBanner />).toJSON();
+
+    const expectedPadding = 'calc(var(--game-box-width) * var(--padding-ratio))';
+    const expectedFontSize = 'calc(var(--game-box-width) * var(--font-size-ratio))';
+    const expectedBorderRadius = 'calc(var(--game-box-width) * var(--border-radius-ratio))';
+
+    expect(campaignBanner).toBeInTheDocument();
+    expect(tree).toHaveStyleRule('top', gameConfig.campaignBanner.offsetTop);
+    expect(tree).toHaveStyleRule('width', '80%');
+    expect(tree).toHaveStyleRule('color', gameConfig.campaignBanner.textColor);
+    expect(tree).toHaveStyleRule('background-color', gameConfig.campaignBanner.background);
+    expect(tree).toHaveStyleRule('border-radius', expectedBorderRadius);
+    expect(tree).toHaveStyleRule('padding', expectedPadding);
+    expect(tree).toHaveStyleRule('font-size', expectedFontSize);
+
+    /**
+     * Calculate the value of css calc() expressions through JS
+     */
+    const cssVars = {
+      '--game-box-width': `${gameConfig.container.maxWidth}px`,
+      '--border-radius-ratio': `${gameConfig.campaignBanner.borderRadius / gameConfig.container.maxWidth}`,
+      '--padding-ratio': `${gameConfig.campaignBanner.padding} / ${gameConfig.container.maxWidth}`,
+      '--font-size-ratio': `${gameConfig.campaignBanner.fontSize} / ${gameConfig.container.maxWidth}`,
+    };
+
+    expect(parseFloat(calc(expectedFontSize, cssVars))).toBeCloseTo(16); // 16px
+    expect(parseFloat(calc(expectedBorderRadius, cssVars))).toBeCloseTo(8); // 8px
+    expect(parseFloat(calc(expectedPadding, cssVars))).toBeCloseTo(12); // 12px
   });
 
   it('SHOULD Render CampaignBanner and maintain responsiveness WHEN viewport width is 250 px', () => {
-    //
+    render(<CampaignBanner />);
+    const campaignBanner = screen.getByTestId(TestElement.CAMPAIGN_BANNER);
+    const tree = renderer.create(<CampaignBanner />).toJSON();
+
+    const expectedPadding = 'calc(var(--game-box-width) * var(--padding-ratio))';
+    const expectedFontSize = 'calc(var(--game-box-width) * var(--font-size-ratio))';
+    const expectedBorderRadius = 'calc(var(--game-box-width) * var(--border-radius-ratio))';
+
+    expect(campaignBanner).toBeInTheDocument();
+    expect(tree).toHaveStyleRule('border-radius', expectedBorderRadius);
+    expect(tree).toHaveStyleRule('padding', expectedPadding);
+    expect(tree).toHaveStyleRule('font-size', expectedFontSize);
+
+    /**
+     * Calculate the value of css calc() expressions through JS
+     */
+    const VIEWPORT_WIDTH = 250;
+
+    const cssVars = {
+      '--game-box-width': `${VIEWPORT_WIDTH}px`,
+      '--border-radius-ratio': `${gameConfig.campaignBanner.borderRadius / gameConfig.container.maxWidth}`,
+      '--padding-ratio': `${gameConfig.campaignBanner.padding} / ${gameConfig.container.maxWidth}`,
+      '--font-size-ratio': `${gameConfig.campaignBanner.fontSize} / ${gameConfig.container.maxWidth}`,
+    };
+
+    expect(parseFloat(calc(expectedFontSize, cssVars))).toBeCloseTo(11.111); // 11.111px
+    expect(parseFloat(calc(expectedBorderRadius, cssVars))).toBeCloseTo(5.555); // 5.555px
+    expect(parseFloat(calc(expectedPadding, cssVars))).toBeCloseTo(8.333); //  8.333px
   });
 
   it('SHOULD Render CampaignBanner and maintain responsiveness WHEN when viewport height is 600 px', () => {
-    //
+    render(<CampaignBanner />);
+    const campaignBanner = screen.getByTestId(TestElement.CAMPAIGN_BANNER);
+    const tree = renderer.create(<CampaignBanner />).toJSON();
+
+    const expectedPadding = 'calc(var(--game-box-width) * var(--padding-ratio))';
+    const expectedFontSize = 'calc(var(--game-box-width) * var(--font-size-ratio))';
+    const expectedBorderRadius = 'calc(var(--game-box-width) * var(--border-radius-ratio))';
+
+    expect(campaignBanner).toBeInTheDocument();
+    expect(tree).toHaveStyleRule('border-radius', expectedBorderRadius);
+    expect(tree).toHaveStyleRule('padding', expectedPadding);
+    expect(tree).toHaveStyleRule('font-size', expectedFontSize);
+
+    /**
+     * Calculate the value of css calc() expressions through JS
+     */
+    const VIEWPORT_HEIGHT = 600;
+    const VIEWPORT_WIDTH = VIEWPORT_HEIGHT * gameConfig.container.ratio;
+
+    const cssVars = {
+      '--game-box-width': `${VIEWPORT_WIDTH}px`,
+      '--border-radius-ratio': `${gameConfig.campaignBanner.borderRadius / gameConfig.container.maxWidth}`,
+      '--padding-ratio': `${gameConfig.campaignBanner.padding} / ${gameConfig.container.maxWidth}`,
+      '--font-size-ratio': `${gameConfig.campaignBanner.fontSize} / ${gameConfig.container.maxWidth}`,
+    };
+
+    expect(parseFloat(calc(expectedFontSize, cssVars))).toBeCloseTo(15); // 15px
+    expect(parseFloat(calc(expectedBorderRadius, cssVars))).toBeCloseTo(7.5); // 7.5px
+    expect(parseFloat(calc(expectedPadding, cssVars))).toBeCloseTo(11.25); // 11.25px
   });
 });
---
+++ src/utils/calc.ts
@@ -0,0 +1,40 @@
+import { calc as cssCalc } from '@csstools/css-calc';
+
+/**
+ * Replaces CSS variable references in a given CSS string with their corresponding values.
+ *
+ * @param {string} cssString - The input CSS string that may contain CSS variable references.
+ * @param {Record<string, string>} vars - An object representing CSS variable names and their corresponding values.
+ * @returns {string} - The modified CSS string with variable references replaced by their values.
+ *
+ * @example
+ * const inputCSS = 'calc(var(--game-box-width) * var(--border-radius-ratio))';
+ * const variables = { '--game-box-width': '360px', '--border-radius-ratio': '0.02222222222' };
+ * const outputCSS = replaceWithValue(inputCSS, variables);
+ *
+ * // Result: 'calc(360px * 0.02222222222)'
+ */
+export function replaceWithActualValue(cssString: string, vars: Record<string, string>): string {
+  for (const key in vars) {
+    cssString = cssString.replace(`var(${key})`, vars[key]);
+  }
+  return cssString;
+}
+
+/**
+ * Computes a CSS calc() expression programatically
+ *
+ * @param {string} cssString - The input CSS string for which we want to compute the value
+ * @param {Record<string, string>} vars - An object representing CSS variable names and their corresponding values.
+ * @returns string - The computed value of the calc() expression
+ *
+ * @example
+ * const inputCSS = 'calc(var(--game-box-width) * var(--border-radius-ratio))';
+ * const variables = { '--game-box-width': '360px', '--border-radius-ratio': '0.02222222222' };
+ * const outputCSS = replaceWithValue(inputCSS, variables);
+ *
+ * // Result: '8px'
+ */
+export function calc(cssString: string, vars: Record<string, string>) {
+  return cssCalc(replaceWithActualValue(cssString, vars));
+}
